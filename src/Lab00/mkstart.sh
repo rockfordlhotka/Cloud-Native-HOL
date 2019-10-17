ab3e0a364019fb6113ce2f305fc2de21b6977010
Rockford Lhotka <rocky@lhotka.net>
2019-10-17 14:44:34 -0500

    #19 Explicitly Use K8s version 1.15.4

--- src/Lab00/mkstart.sh
+++ src/Lab00/mkstart.sh
@@ -1,1 +1,1 @@
-winpty minikube start --vm-driver hyperv --hyperv-virtual-switch "Default Switch" --cpus 6 --memory 4096
+winpty minikube start --vm-driver hyperv --hyperv-virtual-switch "Default Switch" --cpus 6 --memory 4096 --kubernetes-version=1.15.4
