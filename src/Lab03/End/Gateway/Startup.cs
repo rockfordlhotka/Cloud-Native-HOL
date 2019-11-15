4c655ccf40c8932a1ab74ac8e65ca8d80c54bfe8
Rockford Lhotka <rocky@lhotka.net>
2019-11-15 12:47:24 -0600

    Update to netcore30 projects and container images

--- src/Lab03/End/Gateway/Startup.cs
+++ src/Lab03/End/Gateway/Startup.cs
@@ -4,31 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Gateway
 {
   public class Startup
   {
-    public Startup(IConfiguration c) => this.Configuration = c;
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
 
     public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.Configure<CookiePolicyOptions>(options =>
-      {
-              // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-              options.CheckConsentNeeded = context => true;
-        options.MinimumSameSitePolicy = SameSiteMode.None;
-      });
+      services.AddRazorPages();
+      services.AddMvc();
 
-
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
       services.AddSingleton<Services.IWorkInProgress>((e) => new Services.WorkInProgress());
       services.AddHostedService<Services.SandwichmakerListener>();
       services.AddSingleton<Services.ISandwichRequestor>((e) =>
@@ -38,7 +34,7 @@ namespace Gateway
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
       {
@@ -50,9 +46,12 @@ namespace Gateway
       }
 
       app.UseStaticFiles();
-      app.UseCookiePolicy();
-
-      app.UseMvc();
+      app.UseRouting();
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapRazorPages();
+      });
     }
   }
 }
