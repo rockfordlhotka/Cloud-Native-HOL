06e4b682b7aa10bb318924d746aa2891643ce6af
Rockford Lhotka <rocky@lhotka.net>
2019-11-15 12:06:37 -0600

    Update to netcore30 projects and container images

--- src/Lab03/Start/Gateway/Startup.cs
+++ src/Lab03/Start/Gateway/Startup.cs
@@ -4,37 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Gateway
 {
   public class Startup
   {
-    public Startup(IConfiguration c) => this.Configuration = c;
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
 
     public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.Configure<CookiePolicyOptions>(options =>
-      {
-              // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-              options.CheckConsentNeeded = context => true;
-        options.MinimumSameSitePolicy = SameSiteMode.None;
-      });
+      services.AddRazorPages();
 
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
       services.AddSingleton<Services.IWorkInProgress>(new Services.WorkInProgress());
       // TODO: add SandwichmakerListenerHostedService service
       // TODO: add SandwichRequestor service
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
       {
@@ -46,9 +42,15 @@ namespace Gateway
       }
 
       app.UseStaticFiles();
-      app.UseCookiePolicy();
 
-      app.UseMvc();
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapRazorPages();
+      });
     }
   }
 }
