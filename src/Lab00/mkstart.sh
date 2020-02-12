77e21eeb71cfa1008e41ee133677d2274085442f
Rockford Lhotka <rocky@lhotka.net>
2020-02-11 17:24:59 -0800

    Remove version restriction

--- src/Lab00/mkstart.sh
+++ src/Lab00/mkstart.sh
@@ -1,1 +1,1 @@
-winpty minikube start --vm-driver hyperv --hyperv-virtual-switch "Default Switch" --cpus 6 --memory 4096 --kubernetes-version=1.15.4
+winpty minikube start --vm-driver hyperv --hyperv-virtual-switch "Default Switch" --cpus 6 --memory 4096
